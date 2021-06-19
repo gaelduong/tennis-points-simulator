325e8fae1dc3f1bed53ddd04e0bedde5a21ecb42
Gael Duong <mr_jupipro@yahoo.com>
2021-06-19 15:20:44 +0700

    Send data from client's textarea to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,52 @@
 import "./App.css";
 import axios from "axios";
+import { useState } from "react";
 
 const serverUrl = "http://localhost:5000";
 
+const filterAndBuildData = (text) => {
+  const data = [];
+  // Filter
+  let lines = text.split("\n");
+  lines = lines.map((line) => {
+    line = line.split(",");
+    line.forEach((attr, idx) => {
+      line[idx] = attr.replace(/\s/g, "");
+    });
+    // Build data
+    const [shotType, direction, depth, opn_contact_dist] = line;
+    data.push({
+      shotType,
+      direction,
+      depth,
+      opn_contact_dist,
+    });
+  });
+  return data;
+};
+
 function App() {
+  const [text, setText] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const shotsData = {
-      shots: [
-        {
-          player: 1,
-          direction: "right",
-          depth: "short",
-          opn_contact_dist: "normal",
-        },
-        {
-          player: 2,
-          direction: "right",
-          depth: "long",
-          opn_contact_dist: "far",
-        },
-      ],
-    };
+    const shotsData = filterAndBuildData(text);
+    // console.log(shotsData);
     const { data } = await axios.post(`${serverUrl}/simulate`, shotsData);
-
-    console.log(data);
+    // console.log(data);
   };
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
+        <label>
+          Follow this order: shot type, direction, depth, receiving position
+        </label>
+        <label>e.g: fh, far-left, somewhat-short, normal</label>
+        <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
         <input type="submit" value="Simulate" />
       </form>
       <div> Tennis court </div>
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -9,7 +10,17 @@ app.get("/", (req, res) => {
 });
 
 app.post("/simulate", (req, res) => {
-  res.json("data");
+  const { shots } = req.body;
+  const framesData = [
+    {
+      currPlayerPos: [1, 1],
+      currOponnentPos: [1, 1],
+      landSpotPos: [1, 1],
+      receivingContactPos: [1, 1],
+    },
+  ];
+
+  res.json(framesData);
 });
 
 app.listen(5000, () => console.log("Server listening on port 5000"));
