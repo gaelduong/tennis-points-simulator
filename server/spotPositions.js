c93365df66acdb0cc55a716dd15115620eae1fcc
Gael Duong <mr_jupipro@yahoo.com>
2021-06-19 18:17:35 +0700

    Spot positions

---
+++ server/spotPositions.js
@@ -0,0 +1,31 @@
+const spotPositions = {
+  player1StartPosition: [300, 590],
+  player2StartPosition: [170, 50],
+  directionX: {
+    farLeft: 150,
+    left: 190,
+    middle: 250,
+    right: 310,
+    farRight: 350,
+  },
+  depthY: {
+    upSide: {
+      dropshot: 270,
+      short: 220,
+      somewhatShort: 180,
+      normal: 150,
+      somewhatDeep: 110,
+      deep: 90,
+    },
+    downSide: {
+      dropshot: 380,
+      short: 430,
+      somewhatShort: 470,
+      normal: 500,
+      somewhatDeep: 540,
+      deep: 560,
+    },
+  },
+};
+
+module.exports = spotPositions;
