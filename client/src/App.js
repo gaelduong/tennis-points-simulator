f40cc658ed59b04b7b72d7dc23321d9adf1ae55f
Gael Duong <mr_jupipro@yahoo.com>
2021-06-19 14:38:17 +0700

    Setup communication between client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
+import axios from "axios";
+
+const serverUrl = "http://localhost:5000";
 
 function App() {
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const shotsData = {
+      shots: [
+        {
+          player: 1,
+          direction: "right",
+          depth: "short",
+          opn_contact_dist: "normal",
+        },
+        {
+          player: 2,
+          direction: "right",
+          depth: "long",
+          opn_contact_dist: "far",
+        },
+      ],
+    };
+    const { data } = await axios.post(`${serverUrl}/simulate`, shotsData);
+
+    console.log(data);
   };
 
   return (
