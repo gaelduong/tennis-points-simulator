185ba5e8f18d7199ac99d7ae2521a79c98731257
Gael Duong <mr_jupipro@yahoo.com>
2021-06-20 20:51:50 +0700

    Add instruction court

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Court from "./Court.js";
 import ShotsInputForm from "./ShotsInputForm.js";
 
@@ -11,10 +11,25 @@ function App() {
   const [landSpotPos, setLandSpotPos] = useState([]);
   const [receivingContactPos, setReceivingContactPos] = useState([]);
   const [framesData, setFramesData] = useState([]);
-  const [currentFrameId, setCurrentFrameId] = useState(0);
+  const [currentFrameId, setCurrentFrameId] = useState(-1);
+
+  useEffect(() => {
+    if (!framesData.length) return;
+    const currentFrame = framesData[0];
+    if (currentFrame) {
+      setHittingPlayer(currentFrame.hittingPlayer);
+      setShotTypeData(currentFrame.shotTypeData);
+      setPlayer1Pos(currentFrame.player1Pos);
+      setPlayer2Pos(currentFrame.player2Pos);
+      setLandSpotPos(currentFrame.landSpotPos);
+      setReceivingContactPos(currentFrame.receivingContactPos);
+    }
+    setCurrentFrameId(0);
+  }, [framesData]);
 
   const resetFrame = () => {
-    setCurrentFrameId(0);
+    setFramesData([]);
+    setCurrentFrameId(-1);
     setHittingPlayer(-1);
     setPlayer1Pos([300, 590]);
     setPlayer2Pos([170, 50]);
@@ -24,13 +39,15 @@ function App() {
 
   const handleFrameChange = (e, k) => {
     if (!framesData.length) return;
-    if (currentFrameId === 0 && k === -1) return resetFrame();
-    if (currentFrameId === framesData.length - 1 && k === 1) {
-      return resetFrame();
-    }
+    if (currentFrameId <= 0 && k === -1) return;
+    // if (currentFrameId === framesData.length - 1 && k === 1) {
+    //   return;
+    // }
     setCurrentFrameId((currentFrameId) => {
-      currentFrameId += k;
+      currentFrameId =
+        currentFrameId + k >= framesData.length ? 0 : currentFrameId + k;
       const currentFrame = framesData[currentFrameId];
+      console.log(currentFrameId);
       if (currentFrame) {
         setHittingPlayer(currentFrame.hittingPlayer);
         setShotTypeData(currentFrame.shotTypeData);
@@ -74,33 +91,11 @@ function App() {
   return (
     <div className="App">
       <ShotsInputForm setFramesData={setFramesData} resetFrame={resetFrame} />
-      <div>
+      {/* Display dummy court with instructions */}
+      {currentFrameId === -1 && (
         <svg style={style}>
           <Court />
-
-          {hittingPlayer !== -1 && (
-            <g>
-              <line
-                x1={hittingPlayerPos[0]}
-                y1={hittingPlayerPos[1]}
-                x2={landSpotPos[0]}
-                y2={landSpotPos[1]}
-                style={hittingLineStyle}
-              />
-
-              <line
-                x1={landSpotPos[0]}
-                y1={landSpotPos[1]}
-                x2={receivingContactPos[0]}
-                y2={receivingContactPos[1]}
-                style={hittingLineStyle}
-              />
-              <text x={shotTypeData.textPos[0]} y={shotTypeData.textPos[1]}>
-                {shotTypeData.type}
-              </text>
-            </g>
-          )}
-
+          {/* 2 players */}
           <circle
             cx={player1Pos[0]}
             cy={player1Pos[1]}
@@ -113,35 +108,97 @@ function App() {
             r={10}
             style={playerStyle}
           />
+          <text x={10} y={330}>
+            LEFT
+          </text>
+          <text x={440} y={330}>
+            RIGHT
+          </text>
 
-          {hittingPlayer !== -1 && (
-            <g>
-              <circle
-                cx={landSpotPos[0]}
-                cy={landSpotPos[1]}
-                r={3}
-                style={landSpotStyle}
-              />
-              <circle
-                cx={receivingContactPos[0]}
-                cy={receivingContactPos[1]}
-                r={5}
-                style={ballStyle}
-              />
-            </g>
-          )}
+          <text x={230} y={560}>
+            DEEP
+          </text>
+          <text x={230} y={100}>
+            DEEP
+          </text>
+          <text x={280} y={620}>
+            Player 1
+          </text>
+          <text x={140} y={30}>
+            Player 2
+          </text>
         </svg>
+      )}
 
+      {/* Display actual court simulation */}
+      {currentFrameId !== -1 && (
         <div>
+          <svg style={style}>
+            <Court />
+            {/* 2 players */}
+            <circle
+              cx={player1Pos[0]}
+              cy={player1Pos[1]}
+              r={10}
+              style={playerStyle}
+            />
+            <circle
+              cx={player2Pos[0]}
+              cy={player2Pos[1]}
+              r={10}
+              style={playerStyle}
+            />
+            {landSpotPos && (
+              <g>
+                {/* Hitting lines */}
+                <line
+                  x1={hittingPlayerPos[0]}
+                  y1={hittingPlayerPos[1]}
+                  x2={landSpotPos[0]}
+                  y2={landSpotPos[1]}
+                  style={hittingLineStyle}
+                />
+                <line
+                  x1={landSpotPos[0]}
+                  y1={landSpotPos[1]}
+                  x2={receivingContactPos[0]}
+                  y2={receivingContactPos[1]}
+                  style={hittingLineStyle}
+                />
+                {/* Landing spot */}
+                <circle
+                  cx={landSpotPos[0]}
+                  cy={landSpotPos[1]}
+                  r={4}
+                  style={landSpotStyle}
+                />
+                {/* The ball */}
+                <circle
+                  cx={receivingContactPos[0]}
+                  cy={receivingContactPos[1]}
+                  r={7}
+                  style={ballStyle}
+                />
+                {shotTypeData.textPos && (
+                  <text x={shotTypeData.textPos[0]} y={shotTypeData.textPos[1]}>
+                    {shotTypeData.type}
+                  </text>
+                )}
+              </g>
+            )}
+          </svg>
+
           <div>
-            <button onClick={(e) => handleFrameChange(e, -1)}> Back </button>
-            <button onClick={(e) => handleFrameChange(e, 1)}> Next </button>
-          </div>
-          <div>
-            {currentFrameId + 1} / {framesData.length}
+            <div>
+              <button onClick={(e) => handleFrameChange(e, -1)}> Back </button>
+              <button onClick={(e) => handleFrameChange(e, 1)}> Next </button>
+            </div>
+            <div>
+              {currentFrameId + 1} / {framesData.length}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
